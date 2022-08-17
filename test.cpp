647eee82d98bf07fdac1e85afc0673dc6afa110f
HarryYang2002 <80261916+HarryYang2002@users.noreply.github.com>
2022-08-17 23:12:28 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 using namespace std;
 
+void PrintInfo()
+{
+  cout << "My anme" << endl;
+  cout << "DSDSDSDS" << endl;
+}
+
 int main()
 {
+  PrintInfo();
   cout << "Hello World!" << endl;
   return -;
 }
